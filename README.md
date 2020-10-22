f711b56f614f4b33e7db06a42ec4262249d3dea2
deepfrydev <deepfrydev@protonmail.com>
2020-10-22 01:45:58 -0400

    update ketchupV2 contracts

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ https://fry.world.
 
 Feel free to read the code. All updates for new contracts will be added here.
 Including Ketchup Governance 🍅 + Staking contracts
-Fryworld.finance will be under a different repository as it becomes available.
-This repo is only for fry.world frying distribution
+Fryworld.finance core will be under a different repository as it becomes available.
+This repo is only for fry.world token distribution through frying.
 
 Happy Frying 🍟🍟🍟
 
@@ -15,3 +15,5 @@ Happy Frying 🍟🍟🍟
 
 - FriesToken:  https://www.bscscan.com/address/0x393b312c01048b3ed2720bf1b090084c09e408a1
 - DeepFryer:   https://www.bscscan.com/address/0x066d5544a0b05b19f08e45dbc13758a3590386c4
+- KetchupV2: https://www.bscscan.com/token/0x15B3d410fCd0d695E1bbe4F717F8E1B6D0Fb2D0C
+- KetchupV2Proxy: https://www.bscscan.com/address/0x08a785c0c19c131cace73532ebb0f5f6e88462a5
---
+++ contracts/KetchupBar.sol
@@ -0,0 +1,310 @@
+pragma solidity >=0.6.6;
+
+
+import "@deepfrydev/fryworld-farming-lib/contracts/token/BEP20/BEP20.sol";
+
+// KetchupTokenV2 with Governance capabilities.
+contract KetchupBar is BEP20('fry.KetchupV2', 'KHP') {
+
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner. In case of ownership switch.
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+        _moveDelegates(address(0), _delegates[_to], _amount);
+    }
+
+    function burn(address _from ,uint256 _amount) public onlyOwner {
+        _burn(_from, _amount);
+        _moveDelegates(address(0), _delegates[_from], _amount);
+    }
+
+    // The FRIES token
+    FriesToken public fries;
+    // Devs address.
+    address public devAddr;
+
+    constructor(
+        FriesToken _fries,
+        address _devAddr
+    ) public {
+        fries = _fries;
+        devAddr = _devAddr;
+    }
+
+    // Safe fries transfer function, just in case if rounding error causes pool to not have enough FRIES
+    function safeFriesTransfer(address _to, uint256 _amount) public onlyOwner {
+        uint256 friesBal = fries.balanceOf(address(this));
+        if (_amount > friesBal) {
+            fries.transfer(_to, friesBal);
+        } else {
+            fries.transfer(_to, _amount);
+        }
+    }
+
+    // Copied and modified from YAM code:
+    // https://github.com/yam-finance/yam-protocol/blob/master/contracts/token/YAMGovernanceStorage.sol
+    // https://github.com/yam-finance/yam-protocol/blob/master/contracts/token/YAMGovernance.sol
+    // Which is copied and modified from COMPOUND:
+    // https://github.com/compound-finance/compound-protocol/blob/master/contracts/Governance/Comp.sol
+
+    /// @notice A record of each accounts delegate
+    mapping (address => address) internal _delegates;
+
+    /// @notice A checkpoint for marking number of votes from a given block
+    struct Checkpoint {
+        uint32 fromBlock;
+        uint256 votes;
+    }
+
+    // @keep track of players blockNumber on entry
+    mapping (address => uint256) public entryBlock;
+
+    /// @notice A record of votes checkpoints for each account, by index
+    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
+
+    /// @notice The number of checkpoints for each account
+    mapping (address => uint32) public numCheckpoints;
+
+    /// @notice The EIP-712 typehash for the contract's domain
+    bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+
+    /// @notice The EIP-712 typehash for the delegation struct used by the contract
+    bytes32 public constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
+
+    /// @notice A record of states for signing / validating signatures
+    mapping (address => uint) public nonces;
+
+      /// @notice An event thats emitted when an account changes its delegate
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+
+    /// @notice An event thats emitted when a delegate account's vote balance changes
+    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+
+    /**
+     * @notice Delegate votes from `msg.sender` to `delegatee`
+     * @param delegator The address to get delegatee for
+     */
+    function delegates(address delegator)
+        external
+        view
+        returns (address)
+    {
+        return _delegates[delegator];
+    }
+
+   /**
+    * @notice Delegate votes from `msg.sender` to `delegatee`
+    * @param delegatee The address to delegate votes to
+    */
+    function delegate(address delegatee) external {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    /**
+     * @notice Delegates votes from signatory to `delegatee`
+     * @param delegatee The address to delegate votes to
+     * @param nonce The contract state required to match the signature
+     * @param expiry The time at which to expire the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function delegateBySig(
+        address delegatee,
+        uint nonce,
+        uint expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external
+    {
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                DOMAIN_TYPEHASH,
+                keccak256(bytes(name())),
+                getChainId(),
+                address(this)
+            )
+        );
+
+        bytes32 structHash = keccak256(
+            abi.encode(
+                DELEGATION_TYPEHASH,
+                delegatee,
+                nonce,
+                expiry
+            )
+        );
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                structHash
+            )
+        );
+
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "FRIES::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "FRIES::delegateBySig: invalid nonce");
+        require(now <= expiry, "FRIES::delegateBySig: signature expired");
+        return _delegate(signatory, delegatee);
+    }
+
+    /**
+     * @notice Gets the current votes balance for `account`
+     * @param account The address to get votes balance
+     * @return The number of current votes for `account`
+     */
+    function getCurrentVotes(address account)
+        external
+        view
+        returns (uint256)
+    {
+        uint32 nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    /**
+     * @notice Determine the prior number of votes for an account as of a block number
+     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
+     * @param account The address of the account to check
+     * @param blockNumber The block number to get the vote balance at
+     * @return The number of votes the account had as of the given block
+     */
+    function getPriorVotes(address account, uint blockNumber)
+        external
+        view
+        returns (uint256)
+    {
+        require(blockNumber < block.number, "FRIES::getPriorVotes: not yet determined");
+
+        uint32 nCheckpoints = numCheckpoints[account];
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        // First check most recent balance
+        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+            return checkpoints[account][nCheckpoints - 1].votes;
+        }
+
+        // Next check implicit zero balance
+        if (checkpoints[account][0].fromBlock > blockNumber) {
+            return 0;
+        }
+
+        uint32 lower = 0;
+        uint32 upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+            Checkpoint memory cp = checkpoints[account][center];
+            if (cp.fromBlock == blockNumber) {
+                return cp.votes;
+            } else if (cp.fromBlock < blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[account][lower].votes;
+    }
+
+    function _delegate(address delegator, address delegatee)
+        internal
+    {
+        address currentDelegate = _delegates[delegator];
+        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying Fries (not scaled);
+        _delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
+
+    function _moveDelegates(address srcRep, address dstRep, uint256 amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                // decrease old representative
+                uint32 srcRepNum = numCheckpoints[srcRep];
+                uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint256 srcRepNew = srcRepOld.sub(amount);
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                // increase new representative
+                uint32 dstRepNum = numCheckpoints[dstRep];
+                uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint256 dstRepNew = dstRepOld.add(amount);
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    function _writeCheckpoint(
+        address delegatee,
+        uint32 nCheckpoints,
+        uint256 oldVotes,
+        uint256 newVotes
+    )
+        internal
+    {
+        uint32 blockNumber = safe32(block.number, "FRIES::_writeCheckpoint: block number exceeds 32 bits");
+
+        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+            checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+        } else {
+            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+            numCheckpoints[delegatee] = nCheckpoints + 1;
+        }
+
+        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+    function getChainId() internal pure returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
+
+
+    // Enter the staking pool. Stake some FRIES. Earn some extra fries from strongfries, farming pools, and vaults.
+    function enter(uint256 _amount) public {
+        uint256 totalFries = fries.balanceOf(address(this));
+        uint256 totalShares = totalSupply();
+
+        uint256 totalMinusBurn = _amount.mul(98).div(100); // amount minus 2% burn. accounts fries burn. *fix for v2
+
+        if (totalShares == 0 || totalFries == 0) {
+            _mint(msg.sender, totalMinusBurn);
+        } else {
+            uint256 what = totalMinusBurn.mul(totalShares).div(totalFries);
+            _mint(msg.sender, what);
+        }
+        fries.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    // Leave the staking pool. Earn Fries back
+    function leave(uint256 _share) public {
+        uint256 totalShares = totalSupply();
+        uint256 what = _share.mul(fries.balanceOf(address(this))).div(totalShares);
+        _burn(msg.sender, _share);
+        uint256 fee = what.mul(25).div(1000); // 2.5% dev fee to sustain project
+        uint256 stakerAmount = what.sub(fee); // staker - dev fee
+        fries.transfer(devAddr, fee);
+        fries.transfer(msg.sender, stakerAmount);
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devAddr) public {
+        require(msg.sender == devAddr, 'you no fry dev: wut?');
+        devAddr = _devAddr;
+    }
+
+}
